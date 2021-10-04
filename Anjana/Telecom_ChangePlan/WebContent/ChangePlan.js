28c22a543ff3f231b465c09c78f1440bea34a3ba
TimothyZimmermannT <timothy.zimmermann@tcs.com>
2021-10-04 10:40:47 +0200

    working javascript

--- Anjana/Telecom_ChangePlan/WebContent/ChangePlan.js
+++ Anjana/Telecom_ChangePlan/WebContent/ChangePlan.js
@@ -1,7 +1,20 @@
-var form = document.getElementById('form_ChangePlan');
-form.addEventListener('submit', showMessage);
-
 function showMessage(event) {
+	console.log("hello");
   	alert("Your plan has been changed.");
-  	event.preventDefault();
 }
+
+function ready(callback){
+    // in case the document is already rendered
+    if (document.readyState!='loading') callback();
+    // modern browsers
+    else if (document.addEventListener) document.addEventListener('DOMContentLoaded', callback);
+    // IE <= 8
+    else document.attachEvent('onreadystatechange', function(){
+        if (document.readyState=='complete') callback();
+    });
+}
+
+ready(function(){
+    const form = document.getElementById('form_ChangePlan');
+	form.addEventListener('submit', showMessage);
+});
